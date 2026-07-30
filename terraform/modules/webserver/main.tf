17802979f017ab5d949b59d12b635a3cc5e63068
Epicslicey <kaelanmbannister@gmail.com>
2026-07-30 14:37:41 -0700

    Update main.tf

--- terraform/modules/webserver/main.tf
+++ terraform/modules/webserver/main.tf
@@ -72,7 +72,7 @@ resource "aws_iam_instance_profile" "ec2_profile" {
 
 resource "aws_instance" "backend" {
   depends_on = [aws_db_instance.rds, aws_security_group.rds_sg, aws_security_group.ec2-sg]
-  ami           = "ami-0c94855ba95c71c99"
+  ami = "ami-0c7217cdde317cfec"
   subnet_id                   = var.subd_public
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name  # Attach IAM role
   instance_type = "t2.micro"
